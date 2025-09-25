05fe31184acc0bf11e4318610627e239f47442e3
s2005on <129705357+s2005on@users.noreply.github.com>
2025-09-26 04:39:11 +0700

    Add files via upload

---
+++ Thuchanh1/Thuchanh1/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Thuchanh1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            txtUser = new TextBox();
+            txtPass = new TextBox();
+            chkNho = new CheckBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.MenuHighlight;
+            label1.Location = new Point(189, 59);
+            label1.Name = "label1";
+            label1.Size = new Size(124, 23);
+            label1.TabIndex = 0;
+            label1.Text = "Tên đăng nhập";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.MenuHighlight;
+            label2.Location = new Point(189, 102);
+            label2.Name = "label2";
+            label2.Size = new Size(82, 23);
+            label2.TabIndex = 1;
+            label2.Text = "Mật khẩu";
+            // 
+            // txtUser
+            // 
+            txtUser.Location = new Point(334, 59);
+            txtUser.Name = "txtUser";
+            txtUser.Size = new Size(252, 30);
+            txtUser.TabIndex = 2;
+            // 
+            // txtPass
+            // 
+            txtPass.Location = new Point(334, 95);
+            txtPass.Name = "txtPass";
+            txtPass.Size = new Size(252, 30);
+            txtPass.TabIndex = 3;
+            // 
+            // chkNho
+            // 
+            chkNho.AutoSize = true;
+            chkNho.Location = new Point(201, 144);
+            chkNho.Name = "chkNho";
+            chkNho.Size = new Size(93, 27);
+            chkNho.TabIndex = 4;
+            chkNho.Text = "Ghi nhớ";
+            chkNho.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(189, 220);
+            button1.Name = "button1";
+            button1.Size = new Size(114, 29);
+            button1.TabIndex = 5;
+            button1.Text = "Đăng nhập";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(349, 220);
+            button2.Name = "button2";
+            button2.Size = new Size(101, 29);
+            button2.TabIndex = 6;
+            button2.Text = "Xóa";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(492, 220);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 7;
+            button3.Text = "Dừng";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(chkNho);
+            Controls.Add(txtPass);
+            Controls.Add(txtUser);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtUser;
+        private TextBox txtPass;
+        private CheckBox chkNho;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
---
+++ Thuchanh1/Thuchanh1/Form1.cs
@@ -0,0 +1,37 @@
+﻿namespace Thuchanh1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.txtUser.Clear(); this.txtPass.Clear(); this.txtUser.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string thongbao;
+            thongbao = "Tên đăng nhập là: ";
+            thongbao += this.txtUser.Text;
+            thongbao += "\n\rMật khẩu là: ";
+            thongbao += this.txtPass.Text;
+            if (this.chkNho.Checked == true)
+            { thongbao += "\n\rBạn có ghi nhớ."; }
+            MessageBox.Show(thongbao, "Thông báo");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
